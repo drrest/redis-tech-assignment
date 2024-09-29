3d99ff86c1d891bb462426c0816567ececd88e2b
Ihor <ihor.m@ofertirai.me>
2024-09-29 14:25:04 +0300

    feat(main): added comments
    Added comments to improve code readability.

--- main.py
+++ main.py
@@ -14,19 +14,33 @@ logging.basicConfig(level=logging.DEBUG)
 
 processes = []
 def start_consumer(consumer_id):
-    """Запуск одного потребителя"""
+    '''
+    Start consumer
+
+    :param consumer_id:
+    :return:
+    '''
     consumer = ConsumerEngine(consumer_id, redis_host, redis_port)
     consumer.listen_and_process()
 
 def consumer_cleanup(signum, frame):
-    """Удаление всех потребителей из списка при завершении"""
+    """
+    Cleanup function to stop all consumers
+
+    signum: signal - keep
+    frame: frame - keep
+    """
     logging.info("Stopping all consumers...")
     for process in processes:
         process.terminate()
     sys.exit(0)
 
 def create_consumer_group(group_size):
-    """Создаем группу потребителей и запускаем их в разных процессах"""
+    """
+    Run consumer group in separated processes
+
+    :param group_size:
+    """
     for _ in range(int(group_size)):
         consumer_id = str(uuid.uuid4())  # Уникальный ID для каждого потребителя
         process = Process(target=start_consumer, args=(consumer_id,))
@@ -40,18 +54,20 @@ def create_consumer_group(group_size):
 
 
 if __name__ == "__main__":
-    # Парсинг аргументов командной строки
+    # I will parse the command line arguments to allow the user to specify the number of consumers in the group
     parser = argparse.ArgumentParser(description="Конфигурация группы потребителей")
     parser.add_argument('--group_size', type=int, required=False, help="Количество потребителей в группе")
     args = parser.parse_args()
 
+    # If it available in command line i will override default value
     if args.group_size:
         consumers_group_size = int(args.group_size)
 
-    # Регистрация обработчика сигнала
+    # Registrate signals to correct shutdown
     signal.signal(signal.SIGINT, consumer_cleanup)
     signal.signal(signal.SIGTERM, consumer_cleanup)
 
+    # I starting ...
     create_consumer_group(consumers_group_size)
 
 
